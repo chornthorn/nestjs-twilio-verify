86f05248c6c3a8738199f1cf1be032da6c752b10
Chorn Thorn <chornthorn.kh@gmail.com>
2021-04-22 04:30:02 +0700

    add transform interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './utilities/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalInterceptors(new TransformInterceptor())
   await app.listen(3000);
 }
 bootstrap();
